c8686471d2158fa15c33fb4188d268c3f54d19bd
Bashir Muhammed <bashmufol@gmail.com>
2025-06-16 18:43:07 +0100

    Code updated

--- src/main/java/com/bash/todoapp/controller/ToDoController.java
+++ src/main/java/com/bash/todoapp/controller/ToDoController.java
@@ -1,13 +1,20 @@
 package com.bash.todoapp.controller;
 
+import com.bash.todoapp.repository.ToDoRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ToDoController {
 
+    private final ToDoRepo toDoRepo;
+
     @GetMapping({"","/","/home"})
-    public String homePage() {
+    public String homePage(Model model) {
+        model.addAttribute("todos", toDoRepo.findAll());
         return "index";
     }
 }
--- src/main/java/com/bash/todoapp/repository/ToDoRepo.java
+++ src/main/java/com/bash/todoapp/repository/ToDoRepo.java
@@ -1,7 +1,9 @@
 package com.bash.todoapp.repository;
 
+import com.bash.todoapp.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ToDoRepo {
+public interface ToDoRepo extends JpaRepository<TodoEntity, Long> {
 }
