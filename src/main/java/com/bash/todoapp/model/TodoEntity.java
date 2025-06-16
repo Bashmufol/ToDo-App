d1823552d0da9310ff689822e829f6853312709f
Bashir Muhammed <bashmufol@gmail.com>
2025-06-16 18:22:10 +0100

    Entity and Repository classes are created

--- src/main/java/com/bash/todoapp/model/TodoEntity.java
+++ src/main/java/com/bash/todoapp/model/TodoEntity.java
@@ -1,5 +1,22 @@
 package com.bash.todoapp.model;
 
-public class TodoEntity {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tbl_todos")
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false, unique = true)
+    private String title;
+    private Boolean Completed;
 }
