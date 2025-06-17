be6c2155dc119e64da5cb9262d0a214767df98ae
Bashir Muhammed <bashmufol@gmail.com>
2025-06-17 06:15:50 +0100

    Added update and delete APIs

--- src/main/java/com/bash/todoapp/controller/ToDoController.java
+++ src/main/java/com/bash/todoapp/controller/ToDoController.java
@@ -1,10 +1,11 @@
 package com.bash.todoapp.controller;
 
+import com.bash.todoapp.model.TodoEntity;
 import com.bash.todoapp.repository.ToDoRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +17,32 @@ public class ToDoController {
     public String homePage(Model model) {
         model.addAttribute("todos", toDoRepo.findAll());
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestParam String title) {
+        TodoEntity newTodo = TodoEntity.builder()
+                .title(title)
+                .Completed(false)
+                .build();
+        toDoRepo.save(newTodo);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id) {
+        TodoEntity existingTodo = toDoRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("ToDo not found: "+id));
+        existingTodo.setCompleted(true);
+        toDoRepo.save(existingTodo);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        TodoEntity existingTodo = toDoRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("ToDo not found: "+id));
+        toDoRepo.delete(existingTodo);
+        return "redirect:/home";
     }
 }
